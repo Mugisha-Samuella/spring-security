c588264cc6697835f495bbcd9759f31ceecf2604
Mugisha Samuella <itsmugishasamuella@gmail.com>
2025-01-30 21:51:06 +0200

    Security Config

--- src/main/java/com/jetbrains/springsecurityapp/HelloWorld.java
+++ src/main/java/com/jetbrains/springsecurityapp/HelloWorld.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class HelloWorld {
+
+    @GetMapping("/")
+    public String homepage(){
+        return "Welcome to Spring Security App";
+    }
 
     @GetMapping("/hello")
     public String sayHello(){
