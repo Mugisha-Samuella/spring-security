6a9875ee89f92c7b7150e0fecf0764e9e71dc908
Mugisha Samuella <itsmugishasamuella@gmail.com>
2025-01-31 14:28:22 +0200

    role-based access control using @PreAuthorize

--- src/main/java/com/jetbrains/springsecurityapp/HelloWorld.java
+++ src/main/java/com/jetbrains/springsecurityapp/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.jetbrains.springsecurityapp;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,17 @@ public class HelloWorld {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello World";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "Hello User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "Hello Admin!";
     }
 }
--- src/main/java/com/jetbrains/springsecurityapp/SecurityConfig.java
+++ src/main/java/com/jetbrains/springsecurityapp/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jetbrains.springsecurityapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -34,7 +36,7 @@ public class SecurityConfig {
 
         UserDetails admin = User.withUsername("Admin")
                 .password("{noop}admin2")
-                .roles("Admin")
+                .roles("ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(user1, admin);
